3d0a4ec11751d855c09b6af1efe733b94b147a94
12038 <aidan.zachowski@vtsd.com>
2025-05-30 11:12:53 -0400

    updated

--- introduction/Lab03.java
+++ introduction/Lab03.java
@@ -2,22 +2,24 @@ package Lab03;
 
 public class Lab03 {
     public static void main(String[] args) {
+        System.out.println("Lab03, 100 Point Version\n");
 
-        System.out.println("Lab03, 80 point Version\n");
+        int milliseconds = 10000123;
+        int hours = milliseconds / 3600000;
+        int left = milliseconds % 3600000;
+        int minutes = left / 60000;
+        int seconds = (left % 60000) / 1000;
+        int miliseconds = milliseconds % 1000;
 
-        int millisceonds = 10000123;
-        int hours = millisceonds / 3600;
-        int minutes = millisceonds % 3600 / 60;
-        int second = millisceonds % 60;
-        int millisecond = hours / 1000;
-        int minute = hours % 60;
+        System.out.println("Starting milli-seconds: \t" + milliseconds);
+        System.out.println("Hours: \t\t\t\t\t\t" + hours);
+        System.out.println("Minutes: \t\t\t\t\t" + minutes);
+        System.out.println("Seconds: \t\t\t\t\t" + seconds);
+        System.out.println("Milli Seconds: \t\t\t\t" + miliseconds);
 
 
-        System.out.println("Seconds: \t" + second);
-        System.out.println("Minutes: \t" + minutes);
-        System.out.println("Hours: \t\t" + hours);
-        System.out.println("Millisecond: \t" + millisecond);
 
     }
 
 }
+
